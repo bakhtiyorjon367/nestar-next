65ac7e97fdeaae95b95da0df1860e4677cde961a
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-10-09 22:07:33 +0900

    feat: develop likeProperty process within PropertyList screen component

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -10,10 +10,12 @@ import { PropertiesInquiry } from '../../libs/types/property/property.input';
 import { Property } from '../../libs/types/property/property';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
-import { Direction } from '../../libs/enums/common.enum';
-import { useQuery } from '@apollo/client';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { useMutation, useQuery } from '@apollo/client';
 import { GET_PROPERTIES } from '../../apollo/user/query';
 import { T } from '../../libs/types/common';
+import { LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 
 export const getStaticProps = async ({ locale }: any) => ({
 	props: {
@@ -33,7 +35,7 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 	const [sortingOpen, setSortingOpen] = useState(false);
 	const [filterSortName, setFilterSortName] = useState('New');
-
+	const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY);
 	/** APOLLO REQUESTS **/
 	const { 
 		loading:getPropertiesLoading,
@@ -75,6 +77,24 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 		);
 		setCurrentPage(value);
 	};
+
+	const likePropertyHandler = async(user:T, id:string) => {
+		try{
+			//execute getPropertiesRefetch
+			if(!id) return;
+			if(!user._id) throw new Error(Message.NOT_AUTHENTICATED);
+			//likeTargetProperty Mutation
+			await likeTargetProperty({
+				variables:{input: id}
+			});
+			await getPropertiesRefetch({input:initialInput});
+	
+			await sweetTopSmallSuccessAlert("success", 800);
+		}catch(err:any){
+			console.log("ERROR, likeTargetPropertyHandler", err.message);
+			sweetMixinErrorAlert(err.message).then();
+		}
+	}	
 
 	const sortingClickHandler = (e: MouseEvent<HTMLElement>) => {
 		setAnchorEl(e.currentTarget);
@@ -158,7 +178,7 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 									</div>
 								) : (
 									properties.map((property: Property) => {
-										return <PropertyCard property={property} key={property?._id} />;
+										return <PropertyCard property={property} likePropertyHandler={likePropertyHandler} key={property?._id} />;
 									})
 								)}
 							</Stack>

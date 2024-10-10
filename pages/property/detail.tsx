b34e47f833ccfcec9cd649c9a881bfc32bf038d5
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-10-10 19:47:00 +0900

    feat: develop property related comments data

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -33,6 +33,7 @@ import { T } from '../../libs/types/common';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 import { LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
+import { GET_COMMENTS } from '../../apollo/admin/query';
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -90,13 +91,29 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 			sort:"createdAt",
 			direction:Direction.DESC,
 			search:{
-				locationList:[property?.propertyLocation],
+				locationList: property?.propertyLocation ? [property?.propertyLocation]: [],
 			}
 		}},
 		skip:!propertyId && !property,
 		notifyOnNetworkStatusChange:true,
 		onCompleted:(data:T) => {
 			if(data?.getProperties?.list) setDestinationProperties(data?.getProperties?.list);
+		},
+	});
+
+	const { 
+		loading:getCommentsLoading,
+		data:getCommentsData, 
+		error:getCommentsError, 
+		refetch:getCommentsRefetch
+	} = useQuery(GET_COMMENTS, {
+		fetchPolicy:"cache-and-network",
+		variables:{input:initialComment},
+		skip:!commentInquiry.search.commentRefId,
+		notifyOnNetworkStatusChange:true,
+		onCompleted:(data:T) => {
+			if(data?.getCommments?.list) setPropertyComments(data?.getComments?.list);
+			setCommentTotal(data?.getComments?.metaCounter[0]?.total ?? 0);
 		},
 	});
 
@@ -117,7 +134,11 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 		}
 	}, [router]);
 
-	useEffect(() => {}, [commentInquiry]);
+	useEffect(() => {
+		if(commentInquiry.search.commentRefId){
+			getCommentsRefetch({input:commentInquiry});
+		}
+	}, [commentInquiry]);
 
 	/** HANDLERS **/
 	const changeImageHandler = (image: string) => {
